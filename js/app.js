574355a6470360cb8035b21e4fb13561156fd9cc
Mike Pitre <michaelepitre@gmail.com>
2015-09-02 16:06:41 -0500

    appended image to overlay, set image src to clicked image href

--- js/app.js
+++ js/app.js
@@ -1,11 +1,17 @@
 'use strict';
 
-var $overlay = $('<div id="overlay"></div>')
+var $overlay = $('<div id="overlay"></div>');
+var $image = $('<img>');
+
+$overlay.append($image);
+
 
 $('body').append($overlay);
 
 $('#imageGallery a').click(function(event) {
   event.preventDefault();
-  var href = $(this).attr('href');
+  var imageLocation = $(this).attr('href');
+  $image.attr('src', imageLocation);
+
   $overlay.show();
 });
