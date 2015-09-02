971fab66f81be6e4a5abba212c014ec06b002461
Mike Pitre <michaelepitre@gmail.com>
2015-09-02 15:47:36 -0500

    added overlay and overlay styles, shows overlay on click of image

---
+++ js/app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var $overlay = $('<div id="overlay"></div>')
+
+$('body').append($overlay);
+
+$('#imageGallery a').click(function(event) {
+  event.preventDefault();
+  var href = $(this).attr('href');
+  $overlay.show();
+});
