1199d272e536ecfd06d2e2252f3b1e4161a7a201
Mike Pitre <michaelepitre@gmail.com>
2015-09-02 16:36:16 -0500

    added caption to lightbox and made text white

--- js/app.js
+++ js/app.js
@@ -2,9 +2,11 @@
 
 var $overlay = $('<div id="overlay"></div>');
 var $image = $('<img>');
+var $caption = $('<p></p>');
 
 $overlay.append($image);
 
+$overlay.append($caption);
 
 $('body').append($overlay);
 
@@ -14,6 +16,9 @@ $('#imageGallery a').click(function(event) {
   $image.attr('src', imageLocation);
 
   $overlay.show();
+
+  var captionText = $(this).children('img').attr('alt');
+  $caption.text(captionText);
 });
 
 $overlay.click(function(){
